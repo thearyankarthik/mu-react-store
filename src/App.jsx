3f7423a1681e341435b6f14e051b5469caf61d38
thearyankarthik <aryan.karthik2004@gmail.com>
2025-06-09 12:59:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Header'
+import Footer from './Footer'
+import Home from './Home'
+function App() {
+  return (
+    <div> 
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );  
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <>
+        <Header />
+        <App />
+        <Footer />
+
+  </>
+)
